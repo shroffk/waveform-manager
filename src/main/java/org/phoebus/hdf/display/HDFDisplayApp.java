8fd84bec8678aee7ebf490cca27f42009a2dea22
Kunal Shroff <kunalshroff9@gmail.com>
2020-04-12 09:50:59 -0400

    Need to update the classloader path with hdf installation location

--- src/main/java/org/phoebus/hdf/display/HDFDisplayApp.java
+++ src/main/java/org/phoebus/hdf/display/HDFDisplayApp.java
@@ -3,7 +3,10 @@ package org.phoebus.hdf.display;
 import org.phoebus.framework.spi.AppInstance;
 import org.phoebus.framework.spi.AppResourceDescriptor;
 
+import java.lang.reflect.Field;
 import java.net.URI;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -15,13 +18,17 @@ public class HDFDisplayApp implements AppResourceDescriptor {
     static final List<String> SupportedExtensions = List.of(HDF_EXTENSION);
 
     public static final String NAME = "hdf_display";
-    public static final String DISPLAYNAME = "HDF Display View";
+    public static final String DISPLAY_NAME = "HDF Display View";
 
 
     @Override
     public void start() {
         // Load the preferences and configure the dll path
-        System.setProperty("java.library.path", HDFDisplayPreferences.hdf_lib_path);
+        try {
+            addLibraryPath(Paths.get(HDFDisplayPreferences.hdf_lib_path).toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -31,7 +38,7 @@ public class HDFDisplayApp implements AppResourceDescriptor {
 
     @Override
     public AppInstance create(URI resource) {
-        return null;
+        return new HDFDisplay(this);
     }
 
     @Override
@@ -41,17 +48,38 @@ public class HDFDisplayApp implements AppResourceDescriptor {
 
     @Override
     public String getDisplayName() {
-        return DISPLAYNAME;
+        return DISPLAY_NAME;
     }
 
 
     @Override
     public AppInstance create() {
-        return null;
+        return new HDFDisplay(this);
     }
 
     @Override
     public void stop() {
 
+    }
+
+    /**
+     * Add the hdf native binaries to the classloader path.
+     * @param pathToAdd The path to the hdf dlls and binaries
+     * @throws Exception
+     */
+    public static void addLibraryPath(String pathToAdd) throws Exception {
+        logger.info("configuring the class loader path to include " + pathToAdd);
+        Field usrPathsField = ClassLoader.class.getDeclaredField("usr_paths");
+        usrPathsField.setAccessible(true);
+
+        String[] paths = (String[]) usrPathsField.get(null);
+
+        for (String path : paths)
+            if (path.equals(pathToAdd))
+                return;
+
+        String[] newPaths = Arrays.copyOf(paths, paths.length + 1);
+        newPaths[newPaths.length - 1] = pathToAdd;
+        usrPathsField.set(null, newPaths);
     }
 }
