4b5737d8b028d15c376375d531ea7b25761c3451
Kunal Shroff <kunalshroff9@gmail.com>
2020-05-05 11:06:41 -0400

    Add context menu actions

--- src/main/java/org/phoebus/hdf/display/HDFDisplayController.java
+++ src/main/java/org/phoebus/hdf/display/HDFDisplayController.java
@@ -3,14 +3,20 @@ package org.phoebus.hdf.display;
 import hdf.object.h5.H5ScalarDS;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
+import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -23,14 +29,19 @@ import org.csstudio.javafx.rtplot.TraceType;
 import org.csstudio.javafx.rtplot.data.ArrayPlotDataProvider;
 import org.csstudio.javafx.rtplot.data.SimpleDataItem;
 import org.csstudio.javafx.rtplot.util.RGBFactory;
+import org.phoebus.ui.application.Messages;
+import org.phoebus.ui.application.PhoebusApplication;
+import org.phoebus.ui.javafx.ImageCache;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static org.phoebus.hdf.display.HDFFileProcessor.HDFDisplayTreeNode;
 
@@ -53,6 +64,13 @@ public class HDFDisplayController {
     TreeTableColumn plot;
     @FXML
     TreeTableColumn timeStamp;
+
+    // Context menu
+    private final MenuItem addPVs2Plot = new MenuItem("Add PVs to Plot", ImageCache.getImageView(PhoebusApplication.class, "/icons/add.png"));
+    private final MenuItem removePVsFromPlot = new MenuItem("Remove PVs to Plot", ImageCache.getImageView(PhoebusApplication.class, "/icons/delete.png"));
+
+//    private final Menu openWith = new Menu(Messages.OpenWith, ImageCache.getImageView(PhoebusApplication.class, "/icons/fldr_obj.png"));
+    private final ContextMenu contextMenu = new ContextMenu();
 
     // HDF file
     private File file;
@@ -79,6 +97,7 @@ public class HDFDisplayController {
                     protected void updateItem(HDFDisplayTreeNode item, boolean empty) {
                         super.updateItem(item, empty);
                         if (empty) {
+                            setText(null);
                             setGraphic(null);
                         } else {
                             setText(item.getName());
@@ -94,7 +113,9 @@ public class HDFDisplayController {
         plot.setCellFactory(new Callback<TreeTableColumn, TreeTableCell>() {
             @Override
             public TreeTableCell call(TreeTableColumn param) {
+
                 return new TreeTableCell<HDFDisplayTreeNode, HDFDisplayTreeNode>() {
+
 
                     @Override
                     protected void updateItem(HDFDisplayTreeNode item, boolean empty) {
@@ -103,9 +124,8 @@ public class HDFDisplayController {
                             setGraphic(null);
                         } else {
                             if(item.isLeaf()){
-                                CheckBox checkBox = new CheckBox();
+                                final CheckBox checkBox = new CheckBox();
                                 checkBox.selectedProperty().bindBidirectional(item.isPlotted());
-
                                 item.isPlotted().addListener((observable, oldValue, newValue) -> {
                                     if (newValue.booleanValue()) {
                                         addToPlot(item);
@@ -114,18 +134,25 @@ public class HDFDisplayController {
                                     }
                                 });
                                 setGraphic(checkBox);
+                            } else {
+                                setGraphic(null);
                             }
                         }
                     }
                 };
             }
         });
+        treeTableView.getSelectionModel().setSelectionMode(
+                SelectionMode.MULTIPLE
+        );
+
         constructTree();
 
 
         rtTimePlot.setUpdateThrottle(200, TimeUnit.MILLISECONDS);
 
         rtTimePlot.getXAxis().setGridVisible(true);
+        rtTimePlot.getXAxis().setAutoscale(true);
 
         plotArea.getChildren().add(rtTimePlot);
         // anchor to the button
@@ -133,6 +160,30 @@ public class HDFDisplayController {
         plotArea.setLeftAnchor(rtTimePlot, 0.0);
         plotArea.setRightAnchor(rtTimePlot, 0.0);
         plotArea.setBottomAnchor(rtTimePlot, 0.0);
+
+    }
+
+    @FXML
+    public void createContextMenu(ContextMenuEvent e) {
+        System.out.println("context....");
+        final ObservableList<TreeItem<HDFDisplayTreeNode>> selectedItems = treeTableView.selectionModelProperty().getValue().getSelectedItems();
+
+        contextMenu.getItems().clear();
+        addPVs2Plot.setOnAction(event -> {
+            selectedItems.stream().forEach(item -> {
+                item.getValue().isPlotted().set(true);
+            });
+        });
+        removePVsFromPlot.setOnAction(event -> {
+            selectedItems.stream().forEach(item -> {
+                item.getValue().isPlotted().set(false);
+            });
+        });
+        contextMenu.getItems().add(addPVs2Plot);
+        contextMenu.getItems().add(removePVsFromPlot);
+
+        contextMenu.show(treeTableView.getScene().getWindow(), e.getScreenX(), e.getScreenY());
+
 
     }
 
@@ -152,8 +203,9 @@ public class HDFDisplayController {
                 for (int i = 0; i < d1.length; i++) {
                     data.add(new SimpleDataItem<>(item.getInstant().plusMillis(i*1), d1[i]));
                 }
-                traces.put(item.getName(),
-                        rtTimePlot.addTrace(item.getName(), "socks", data, colors.next(), TraceType.LINES, 3, LineStyle.SOLID, PointType.NONE, 3, 0));
+                Trace<Instant> trace = rtTimePlot.addTrace(item.getName(), "socks", data, colors.next(), TraceType.LINES, 3, LineStyle.SOLID, PointType.NONE, 3, 0);
+                traces.put(item.getName(), trace);
+                rtTimePlot.getYAxes().get(trace.getYAxis()).setAutoscale(true);
                 rtTimePlot.requestUpdate();
 
             } catch (Exception e) {
