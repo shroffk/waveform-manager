98c47789715a87e97d1ff1debd717b15382550e8
Kunal Shroff <kunalshroff9@gmail.com>
2020-05-27 14:19:36 -0400

    test and demo for index viewer

---
+++ waveform-index-viewer/src/test/java/org/phoebus/app/waveform/index/viewer/WaveformIndexViewerDemo.java
@@ -0,0 +1,30 @@
+package org.phoebus.app.waveform.index.viewer;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.phoebus.ui.javafx.ApplicationWrapper;
+
+public class WaveformIndexViewerDemo extends ApplicationWrapper {
+
+    public static void main(String[] args) {
+        launch(WaveformIndexViewerDemo.class, args);
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("WaveformIndexViewer.fxml"));
+        loader.load();
+
+        WaveformIndexViewerController controller = loader.getController();
+
+        Parent root = loader.getRoot();
+        stage.setScene(new Scene(root, 400, 400));
+        stage.show();
+
+    }
+}
